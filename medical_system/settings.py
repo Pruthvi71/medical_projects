e2fc7a4e2e85dd9f7eebe39b04e3ae6bddd40aac
Vraj Kheni <khenivraj2007@gmail.com>
2026-05-14 16:35:22 +0530

    email env setup

--- medical_system/settings.py
+++ medical_system/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -137,6 +138,5 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = 'khenivraj2007@gmail.com'
-EMAIL_HOST_PASSWORD = 'dqgycczueacqbews'
-
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
