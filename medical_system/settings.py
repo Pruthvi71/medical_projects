1280a74987e7d63e7a91a7ce9743c7a541c8d701
Vraj Kheni <khenivraj2007@gmail.com>
2026-05-14 17:50:04 +0530

    final email fix

--- medical_system/settings.py
+++ medical_system/settings.py
@@ -136,7 +136,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
