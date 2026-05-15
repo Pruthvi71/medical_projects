b8084ecffe2a6738bc1d7bd9257901775afb42d5
Vraj Kheni <khenivraj2007@gmail.com>
2026-05-15 17:03:36 +0530

    Add deployment diagnostics

--- medical_system/urls.py
+++ medical_system/urls.py
@@ -15,14 +15,29 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
+from django.http import JsonResponse
 from django.urls import path, include
 from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
 
+def deploy_status(request):
+    return JsonResponse({
+        'app': 'VRAJ Care',
+        'version': 'password-reset-diagnostic-a09a566',
+        'email_backend': settings.EMAIL_BACKEND,
+        'email_host': settings.EMAIL_HOST,
+        'email_host_user_set': bool(settings.EMAIL_HOST_USER),
+        'email_password_set': bool(settings.EMAIL_HOST_PASSWORD),
+        'default_from_email': settings.DEFAULT_FROM_EMAIL,
+        'public_site_url': settings.PUBLIC_SITE_URL,
+    })
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('__deploy_status__/', deploy_status, name='deploy_status'),
     path('', home, name='home'),
     path('accounts/', include('accounts.urls')),
     path('appointments/', include('appointments.urls')),
